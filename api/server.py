8c5a5deef3e62a41b82367c2476daef39b476adb
Bill Baumgartner <bill.baumgartner@gmail.com>
2020-11-06 09:31:46 -0700

    Updated API description with documentation

--- api/server.py
+++ api/server.py
@@ -23,8 +23,52 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 
 app = FastAPI(
-    title="Text Mining Provider API",
-    description="REST portal for Text Mining Provider targeted Biolink association database",
+    title="Text Mining Provider -- Targeted text-mined association API",
+    description="""This REST portal serves Biolink associations that have been mined from the scientific literature. <br> 
+                   Current content include the following associations: 
+                   <ul>
+                    <li> <a href="https://biolink.github.io/biolink-model/docs/ChemicalToGeneAssociation.html">biolink:ChemicalToGeneAssociation</a>
+                        <ul>
+                            <li> Chemicals are represented using the <a href="https://www.ebi.ac.uk/chebi/">CHEBI ontology</a>.
+                            <li> Genes are represented as gene products using the <a href="https://proconsortium.org/">Protein Ontology</a>. 
+                            <ul>
+                                <li>Note that the species non-specific classes from the Protein Ontology have been preferred in the annotation process, <br>so you may need to make use of the subsumption hierarchy in the Protein Ontology to link to species-specific entities.
+                            </ul>
+                            <li> Relations between chemicals and genes include:
+                        <ul>
+                            <li> <a href="https://biolink.github.io/biolink-model/docs/positively_regulates_entity_to_entity.html">biolink:positively_regulates_entity_to_entity</a>
+                            <li> <a href="https://biolink.github.io/biolink-model/docs/negatively_regulates_entity_to_entity.html">biolink:negatively_regulates_entity_to_entity</a>
+                        </ul>
+                        <li> Example:
+                        <pre>{
+  &nbsp;&nbsp;"message": {
+    &nbsp;&nbsp;&nbsp;&nbsp;"query_graph": {
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"nodes": [
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"id": "n0",
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"type": "chemical_substance",
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"curie": "CHEBI:3215"
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;},
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"id": "n1",
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"type": "gene_product",
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"curie": "PR:000031567"
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;],
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"edges": [
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"id": "e0",
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"source_id": "n0",
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"target_id": "n1",
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"type": "negatively_regulates_entity_to_entity"
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;]
+    &nbsp;&nbsp;&nbsp;&nbsp;}
+  &nbsp;&nbsp;}
+}
+                        </pre>
+                    </ul>
+                   """,
     version="1.0.0",
 )
 
